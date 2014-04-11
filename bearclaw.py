a556f6549e866564a213dc427d9d1a0a419ed72a
Evan Sangaline <esangaline@gmail.com>
2014-04-11 08:59:05 -0700

    Added a quick hacked together qwerty keyboard and a function for drawing keyboards.

--- bearclaw.py
+++ bearclaw.py
@@ -45,3 +45,78 @@ def DrawPolygons(polylist):
     ax.set_xlim(l-xpad, r+xpad)
     ax.set_ylim(b-ypad, t+ypad)
     plt.show()
+
+def DrawKeyboard(k):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    (l, r, b, t) = (0, 0, 0, 0)
+
+    d = k.PolygonDict()
+
+    for c in d.keys():
+        p = d[c]
+        verts = p.VertexList()
+        codes = [Path.MOVETO] + [Path.LINETO for i in range(len(verts)-2)] + [Path.CLOSEPOLY]
+        path = Path(verts, codes)
+        (newl, newr, newb, newt) = (p.LeftExtreme(), p.RightExtreme(), p.BottomExtreme(), p.TopExtreme())
+        l = min(l, newl)
+        r = max(r, newr)
+        b = min(b, newb)
+        t = max(t, newt)
+
+        patch = patches.PathPatch(path, facecolor='orange', lw=2)
+        ax.add_patch(patch)
+
+        ax.text(newl + 0.2*(newr-newl),newb + 0.2*(newt-newb), c, fontsize=18)
+
+    xpad = (r-l)*0.1
+    ypad = (t-b)*0.1
+    ax.set_xlim(l-xpad, r+xpad)
+    ax.set_ylim(b-ypad, t+ypad)
+    plt.show()
+
+def StandardQwerty():
+    width = 1.0
+    height = 1.5
+    gap = 0.2
+    startx = 0.0
+    starty = 0.0
+    l = []
+    for i in range(10):
+        p = cruller.Polygon()
+        p.AddVertex(startx + float(i)*(width+gap), starty)
+        p.AddVertex(startx + float(i)*(width+gap) + width, starty)
+        p.AddVertex(startx + float(i)*(width+gap) + width, starty-height)
+        p.AddVertex(startx + float(i)*(width+gap), starty-height)
+        l.append(p)
+    startx += 0.5*width
+    starty -= height + gap
+    for i in range(9):
+        p = cruller.Polygon()
+        p.AddVertex(startx + float(i)*(width+gap), starty)
+        p.AddVertex(startx + float(i)*(width+gap) + width, starty)
+        p.AddVertex(startx + float(i)*(width+gap) + width, starty-height)
+        p.AddVertex(startx + float(i)*(width+gap), starty-height)
+        l.append(p)
+    startx += width + gap
+    starty -= height + gap
+    for i in range(7):
+        p = cruller.Polygon()
+        p.AddVertex(startx + float(i)*(width+gap), starty)
+        p.AddVertex(startx + float(i)*(width+gap) + width, starty)
+        p.AddVertex(startx + float(i)*(width+gap) + width, starty-height)
+        p.AddVertex(startx + float(i)*(width+gap), starty-height)
+        l.append(p)
+    starty -= height + gap
+    p = cruller.Polygon()
+    p.AddVertex(startx + float(6)*(width+gap), starty)
+    p.AddVertex(startx + float(6)*(width+gap) + width, starty)
+    p.AddVertex(startx + float(6)*(width+gap) + width, starty-height)
+    p.AddVertex(startx + float(6)*(width+gap), starty-height)
+    l.append(p)
+
+    qwerty = 'qwertyuiopasdfghjklzxcvbnm.'
+    k = cruller.Keyboard()
+    for i in range(27):
+        k.AddKey(qwerty[i], l[i])
+    return k

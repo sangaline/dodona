54b000a6148e0344f43a20f0b17608f1509edd5b
Rylan Conway <RylanC24@gmail.com>
2015-02-02 19:40:43 -0800

    changed the vector access to use .at() to help with debugging

--- cruller/src/InputModels/InputVector.cpp
+++ cruller/src/InputModels/InputVector.cpp
@@ -15,7 +15,7 @@ unsigned int InputVector::Length() {
 unsigned int InputVector::AddPoint(double x, double y, double t) {
     unsigned int i;
     for(i = 0; i < tvector.size(); i++) {
-        if(t < tvector[i]) {
+        if(t < tvector.at(i)) {
             break;
         }
     }
@@ -55,29 +55,29 @@ double InputVector::T(int i) {
 
 void InputVector::SetX(int i, double x) {
     while(i<0) { i += Length(); }
-    xvector[i] = x;
+    xvector.at(i) = x;
 }
 
 void InputVector::SetY(int i, double y) {
     while(i<0) { i += Length(); }
-    yvector[i] = y;
+    yvector.at(i) = y;
 }
 
 void InputVector::SetT(int i, double t) {
     while(i<0) { i += Length(); }
-    tvector[i] = t;
+    tvector.at(i) = t;
 }
 
 double InputVector::SpatialLength() {
     double l = 0;
     for(unsigned int i = 1; i < xvector.size(); i++) {
-        l += sqrt( pow( xvector[i] - xvector[i-1], 2) + pow( yvector[i] - yvector[i-1], 2) );
+        l += sqrt( pow( xvector.at(i) - xvector.at(i-1), 2) + pow( yvector.at(i) - yvector.at(i-1), 2) );
     }
     return l;
 }
 
 double InputVector::TemporalLength() {
-    return tvector[tvector.size()-1] - tvector[0];
+    return tvector.at(tvector.size()-1) - tvector.at(0);
 }
 
 double InputVector::DeltaPhi(unsigned int i) {
@@ -85,8 +85,8 @@ double InputVector::DeltaPhi(unsigned int i) {
     if( i == 0 || i >= xvector.size() - 1 ) {
         return 0;
     }
-    const double oldphi = atan2( (yvector[i]-yvector[i-1]), (xvector[i]-xvector[i-1]) );
-    const double newphi = atan2( (yvector[i+1]-yvector[i]), (xvector[i+1]-xvector[i]) );
+    const double oldphi = atan2( (yvector.at(i)-yvector.at(i-1)), (xvector.at(i)-xvector.at(i-1)) );
+    const double newphi = atan2( (yvector.at(i+1)-yvector.at(i)), (xvector.at(i+1)-xvector.at(i)) );
 
     return newphi - oldphi;
 }
@@ -96,11 +96,11 @@ const char* InputVector::StringForm(Keyboard& k) {
 
     unsigned int lastj = 0;
     for(unsigned int i = 0; i < xvector.size(); i++) {
-        if( i>0 && k.GetKey(k.CharN(lastj)).IsInside(xvector[i], yvector[i]) ) {
+        if( i>0 && k.GetKey(k.CharN(lastj)).IsInside(xvector.at(i), yvector.at(i)) ) {
             continue;
         }
         for(unsigned int j = 0; j < k.NKeys(); j++) {
-            if( k.GetKey(k.CharN(j)).IsInside(xvector[i], yvector[i]) ) {
+            if( k.GetKey(k.CharN(j)).IsInside(xvector.at(i), yvector.at(i)) ) {
                 s.push_back(char(k.CharN(j)));
                 lastj = j;
                 break;

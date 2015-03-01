34b6e93e91f060ac00df039513b33ab3dd936c81
Evan Sangaline <esangaline@gmail.com>
2015-03-01 12:21:25 -0800

    Eliminated the duplicate code in DrawPolygon and DrawPolygons. Also added facecolor and figsize options.

--- keyboards.py
+++ keyboards.py
@@ -21,24 +21,15 @@ from itertools import cycle
 #for adding text later: http://matplotlib.org/1.3.1/users/annotations_intro.html
 
 def DrawPolygon(p):
-    verts = p.VertexList()
-    codes = [Path.MOVETO] + [Path.LINETO for i in range(len(verts)-2)] + [Path.CLOSEPOLY]
-    path = Path(verts, codes)
+    DrawPolygons(p)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    patch = patches.PathPatch(path, facecolor='orange', lw=2)
-    ax.add_patch(patch)
-    (l, r, b, t) = (p.LeftExtreme(), p.RightExtreme(), p.BottomExtreme(), p.TopExtreme())
-    xpad = (r-l)*0.1
-    ypad = (t-b)*0.1
-    ax.set_xlim(l-xpad, r+xpad)
-    ax.set_ylim(b-ypad, t+ypad)
-    plt.show()
-
-
-def DrawPolygons(polylist):
-    fig = plt.figure()
+def DrawPolygons(polylist, facecolor = 'lightblue', figsize = None):
+    if not isinstance(polylist, collections.Iterable):
+        polylist = [polylist]
+    if figsize != None:
+        fig = plt.figure(figsize=figsize)
+    else:
+        fig = plt.figure()
     ax = fig.add_subplot(111)
     (l, r, b, t) = (0, 0, 0, 0)
     for p in polylist:
@@ -51,7 +42,7 @@ def DrawPolygons(polylist):
         b = min(b, newb)
         t = max(t, newt)
 
-        patch = patches.PathPatch(path, facecolor='orange', lw=2)
+        patch = patches.PathPatch(path, facecolor=facecolor, lw=2)
         ax.add_patch(patch)
 
     xpad = (r-l)*0.1
